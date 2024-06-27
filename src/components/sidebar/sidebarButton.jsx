ceab4fd5cb53e5f8086b6d82bfaf36ff8d54aa41
Keith Warren <128069942+keithAwarren@users.noreply.github.com>
2024-06-27 15:06:51 -0230

    sidebar/style update

--- src/components/sidebar/sidebarButton.jsx
+++ src/components/sidebar/sidebarButton.jsx
@@ -1,11 +1,17 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { IconContext } from "react-icons";
 import "./sidebarButton.css";
 
 function SidebarButton(props) {
+    const location = useLocation();
+
+    const isActive = location.pathname === props.to;
+
+    const btnClass = isActive ? "btn-body active": "btn-body";
+    
     return (
         <Link to={props.to}>
-            <div className="btn-body">
+            <div className={btnClass}>
                 <IconContext.Provider value={{ size: "30px", className: "btn-icon"}}>
                 {props.icon}
                 <p className="btn-title">{props.title}</p>
